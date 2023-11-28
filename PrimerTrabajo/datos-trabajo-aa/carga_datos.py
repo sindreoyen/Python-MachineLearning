0129c6e0225caaaab9f1dd4e2ef0d15c5f00070c
Sindre Øyen <31347883+sindreoyen@users.noreply.github.com>
2023-11-28 11:15:59 +0100

    feat: add loading of digits + improvements

--- PrimerTrabajo/datos-trabajo-aa/carga_datos.py
+++ PrimerTrabajo/datos-trabajo-aa/carga_datos.py
@@ -5,7 +5,7 @@
 # "APRENDIZAJE AUTOMÁTICO"
 
 import numpy as np
-
+import os
 
 
 # ----------------------------------------------------
@@ -85,13 +85,65 @@ y_cancer=cancer.target
 #np.save("datos/imdb_sentiment/y_train_text.npy",y_train)
 #np.save("datos/imdb_sentiment/y_test_text.npy",y_test)
 
-X_train_imdb=np.load("datos/imdb_sentiment/vect_train_text.npy")
-X_test_imdb=np.load("datos/imdb_sentiment/vect_test_text.npy")
-y_train_imdb=np.load("datos/imdb_sentiment/y_train_text.npy")
-y_test_imdb=np.load("datos/imdb_sentiment/y_test_text.npy")
+# Find the path to the current file
+file_path = os.path.abspath(os.path.dirname(__file__))
+
+imdb_sentiment_path = os.path.join(file_path, "datos", "imdb_sentiment")
+X_train_imdb=np.load(os.path.join(imdb_sentiment_path, "vect_train_text.npy"))
+X_test_imdb=np.load(os.path.join(imdb_sentiment_path, "vect_test_text.npy"))
+y_train_imdb=np.load(os.path.join(imdb_sentiment_path, "y_train_text.npy"))
+y_test_imdb=np.load(os.path.join(imdb_sentiment_path, "y_test_text.npy"))
 
 # ----------------------------------------------------------------
 
 # DÍGITOS ESCRITOS A MANO
 
-# ver digits.zip
+# Path to the data directory
+digits_path = os.path.join(file_path, "datos", "digitdata")
+
+# The digits dataset
+path_x_train = os.path.join(digits_path, "trainingimages")
+path_y_train = os.path.join(digits_path, "traininglabels")
+path_x_test = os.path.join(digits_path, "testimages")
+path_y_test = os.path.join(digits_path, "testlabels")
+path_x_valid = os.path.join(digits_path, "validationimages")
+path_y_valid = os.path.join(digits_path, "validationlabels")
+
+def print_digit(digit):
+    for line in digit:
+        print("".join([{2: "+", 1: "#", 0: " "}[c] for c in line]))
+
+def load_digits(path_x: str, path_y: str, digit_height=10):
+    with open(path_x) as file_x, open(path_y) as file_y:
+        y = [int(line.rstrip()) for line in file_y]
+        x, current_digit_lines = [], []
+        blank_line_count = 0
+
+        for line in file_x:
+            stripped_line = line.rstrip()
+
+            if stripped_line:
+                # Reset blank line count if a non-blank line is encountered
+                blank_line_count = 0
+                current_digit_lines.append([{"+": 2, "#": 1, " ": 0}[c] for c in stripped_line])
+            else:
+                blank_line_count += 1
+                # Add the digit if it has reached the expected height and is followed by blank lines
+                if len(current_digit_lines) >= digit_height and blank_line_count > 1:
+                    x.append(current_digit_lines)
+                    current_digit_lines = []
+
+        # Add the last digit if it meets the height requirement
+        if len(current_digit_lines) >= digit_height:
+            x.append(current_digit_lines)
+
+    return x, np.array(y, dtype=int)
+
+# With some research of the data, the digits all appear to be min 10 in height
+X_train_digits, y_train_digits = load_digits(path_x_train, path_y_train, digit_height=10)
+X_test_digits, y_test_digits = load_digits(path_x_test, path_y_test, digit_height=10)
+X_valid_digits, y_valid_digits = load_digits(path_x_valid, path_y_valid, digit_height=10)
+
+print(len(X_train_digits), len(y_train_digits))
+print(len(X_test_digits), len(y_test_digits))
+print(len(X_valid_digits), len(y_valid_digits))
